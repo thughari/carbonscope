b2eafd7d9bbbe5e72caae72c3541f2284208d53c
thughari <thughari3@gmail.com>
2025-12-08 00:38:45 +0530

    added exception handler

---
+++ src/main/java/com/carbonscope/dto/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.carbonscope.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ErrorResponse {
+	private int status;
+    private String message;
+}
---
+++ src/main/java/com/carbonscope/handler/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.carbonscope.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.carbonscope.dto.ErrorResponse;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+    @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "An unexpected error occurred.");
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
