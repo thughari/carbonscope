b9c81ceb8fe62ab9a617c87bf1fdeadfa8db24a7
thughari <thughari3@gmail.com>
2025-12-31 08:55:55 +0530

    added json ignore

--- src/main/java/com/carbonscope/model/EmissionSource.java
+++ src/main/java/com/carbonscope/model/EmissionSource.java
@@ -1,8 +1,11 @@
 package com.carbonscope.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class EmissionSource {
 	private long id;
 	private String name;
